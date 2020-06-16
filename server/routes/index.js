7fe49a18ea6bdf22c4099b6454e2928379cb168d
chiragdoctor <chirag.doctor@gmail.com>
2020-06-16 20:36:51 +0100

    get users

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 const User = require('../models/user');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.json({ msg: 'hello world' });
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  res.json({ users });
 });
 
 router.post('/', async (req, res) => {
